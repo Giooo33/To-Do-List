f6f3892acd6f60ce510cd9a0d7e176ece9343cb9
Kkraties13 <160873692+Kkraties13@users.noreply.github.com>
2025-09-23 21:58:29 -0300

    Update vite.config.js
    Configurando o site

--- App/frontend/To-do-List/vite.config.js
+++ App/frontend/To-do-List/vite.config.js
@@ -6,6 +6,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "To-Do-List",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
